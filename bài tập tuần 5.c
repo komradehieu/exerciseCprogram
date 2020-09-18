a6b2d207f43a16214f4afbcae9c5fdc5a2919b27
komradehieu <69588235+komradehieu@users.noreply.github.com>
2020-09-18 16:17:02 +0700

    Update bài tập tuần 5.c

--- bài tập tuần 5.c
+++ bài tập tuần 5.c
@@ -1,17 +1,17 @@
-cho 1 dãy số nguyên gồm n phần tử với n nguyên dương và nhập đc từ bàn phím
-+ khai báo & nhập data cho dãy
-+ tính tổng các phần tử trong dãy đó
-+ tìm giá trị của phần tử max
-+ đếm số phần tử nhận giá trị âm
-+ sắp xếp dãy đã cho theo trật tự tăng dần
+//cho 1 dãy số nguyên gồm n phần tử với n nguyên dương và nhập đc từ bàn phím
+//+ khai báo & nhập data cho dãy
+//+ tính tổng các phần tử trong dãy đó
+//+ tìm giá trị của phần tử max
+//+ đếm số phần tử nhận giá trị âm
+//+ sắp xếp dãy đã cho theo trật tự tăng dần
 
 
 
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #include <stdio.h>
 int main()
-{
+     {
 
 int A[10];
 int i, n;
@@ -20,8 +20,41 @@ printf("\nNhap so phan tu cua day\n");
 scanf("%d",&int);
 printf("---------------------------------");
 
-for(i=1; i<=n; i
-printf("cho biet phan tu thu %d:",i);
-scanf("%d", &A[i]);
+  for(i=1; i<=n; i++)
+        {
+  
+            printf("cho biet phan tu thu %d:",i);
+            scanf("%d", &A[i]);
+        }
 
-}
+
+
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------------
+***** BTVN : cho ma trận a cấp nxm với n và m là các số nguyên nhập vào từ bàn phím****
+1. khai báo nhập data cho ma trận A
+2. in các phần tử của ma trận vừa nhập = 2 cách
+      + in theo thứ tự mình nhập
+      + in theo đúng hình ảnh ma trận 
+         x  x  x
+         y  y  y
+         z  z  z
+3. tìm phần tử lớn nhất của ma trận đã nhập ( 2 vl lồng nhau)
+4. tính tổng các phần tử nằm trên đường chéo chính của ma trận ( phần tử có dạng Aii) ( ma trận vuông mới tính)
+---------------------------------------------------------------------------------------------------------------------------
+         
+  
+
